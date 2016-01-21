12f5e0d79f1927d9c222f7a12c5d46de6983345c
Charlie Egan <git@charlieegan3.com>
2016-01-21 00:19:30 +0000

    Improve summary display information on index page
    Motivation:
    -reddit threads were poorly displayed
    
    Change Explanation
    -now the summary can be built from the root for the first response using
    the summary_text method on the comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,20 @@
 class Comment < ActiveRecord::Base
   has_many :children, class_name: "Comment", foreign_key: "parent_id"
   belongs_to :parent, class_name: 'Comment'
+
+  def domain
+    source.split("/")[2]
+  end
+
+  def summary_text
+    text = body.split(/\s+/).take(20).join(" ")
+    if text.length < 30
+      text += "Comment: "
+      text += children.first.body.split(/\s+/).take(20).join(" ")
+    end
+    if text.length > 70
+      text = text[0..67] + "..."
+    end
+    return text
+  end
 end
