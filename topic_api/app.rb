e2bd4a6befef0e2662a7feb5eeeb33c18a8c63bb
Charlie Egan <git@charlieegan3.com>
2016-01-24 22:38:13 +0000

    Implement simple topic modeling API
    Motivation:
    -topic modeling is the first step in points extraction
    
    Change Explanation:
    -expose lda gem with a simple Sinatra service with a single POST
    endpoint
    -service returns topics and topic groups for a given text input
    -counts of topics and groups can be passed as parameters

---
+++ topic_api/app.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'lda-ruby'
+require 'json'
+
+set :port, ENV['PORT']
+set :bind, '0.0.0.0'
+
+class TopicAnalyzer
+  def initialize(text, topic_count)
+    corpus = Lda::Corpus.new
+    corpus.add_document(Lda::TextDocument.new(corpus, text))
+    @lda = Lda::Lda.new(corpus)
+    @lda.num_topics = topic_count
+    @lda.em('random')
+  end
+
+  def top_words(count)
+    @lda.top_words(count).values
+  end
+end
+
+post '/' do
+  payload = JSON.parse(request.body.read)
+  text = payload["text"].encode("UTF-8", :invalid=>:replace, :replace=>"?")
+
+  ta = TopicAnalyzer.new(text, payload["topic_count"].to_i)
+  groups = ta.top_words(payload["top_word_count"].to_i)
+
+  {
+    topics: groups.flatten.uniq.sort,
+    groups: groups
+  }.to_json
+end
