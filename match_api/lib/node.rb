4f1f9abc05d7a12e071fa457f63e3b88f943d0a6
Charlie Egan <git@charlieegan3.com>
2016-02-06 14:35:43 +0000

    Implement simple point decomposition
    Motivation:
    -Attempt to cover Apposition and Coordinating Conjunctions
    
    Change Explanation:
    -a point that is centered on a verb with more than one outgoing nmod:x
    is broken down into a series of shorter points.
    -later dependents of nmod:to relations from the parent verb have a case
    added to match the nmod:to relation if there is one missing

--- match_api/lib/node.rb
+++ match_api/lib/node.rb
@@ -31,10 +31,49 @@ class Node
     end.flatten.uniq
   end
 
+  def add_case
+    return self if (nmod_egdes = matching_edges(/nmod:/, true, false)).size != 1
+    return self if matching_edges(/case/, false, true).size != 0
+    nmod = nmod_egdes.first.label.split(":").last
+    case_node = Node.new(nmod, nmod.upcase, nmod, index-0.1)
+    outbound << Edge.new(self, case_node, "case")
+    case_node.inbound << Edge.new(self, case_node, "case")
+    return [self, case_node]
+  end
+
+  def nmod_indexes
+    outbound.map { |e| /nmod:/ =~ e.label }
+      .each_with_index
+      .map { |e, i| i if e }
+      .compact
+  end
+
+  def nmod_points
+    sets = []
+    indexes = nmod_indexes
+    return [] if indexes.size < 2
+    indexes.size.times do
+      set = outbound.dup
+      set.delete_at(indexes.shift)
+      sets << set
+    end
+    sets.map do |set|
+      set.map do |e|
+        [
+          e.destination,
+          e.destination.descendants(/^((?!^cc$|^conj:and).)*$/)
+        ].flatten.reject { |n| n.pos.match(/CC|RB/) }.map(&:add_case)
+      end.flatten << self
+    end.reverse
+  end
+
   def points
-    [
-      descendants << self,
-      descendants(/^((?!^cc$|^conj:and).)*$/) << self,
-    ].uniq
+    [].tap do |points|
+      points << (descendants << self)
+      if nmod_indexes.size == 1
+        points << (descendants(/^((?!^cc$|^conj:and).)*$/) << self)
+      end
+      nmod_points.map { |p| points << p }
+    end.uniq
   end
 end
--- match_api/test/node_test.rb
+++ match_api/test/node_test.rb
@@ -28,4 +28,40 @@ class TestNode < Test::Unit::TestCase
     assert_equal([n1], n2.ancestors)
     assert_equal([n1, n2].reverse, n3.ancestors)
   end
+
+  def test_add_case
+    node = Node.new("word", "pos", "lemma", 0)
+    node.inbound << Edge.new(nil, node, "nmod:to")
+
+    assert_equal(nil, node.outbound.map(&:label).first)
+    node.add_case
+    assert_equal('case', node.outbound.map(&:label).first)
+    assert_equal('to', node.outbound.first.destination.word)
+  end
+
+  def test_nmod_indexes
+    node = Node.new("word", "pos", "lemma", 0)
+
+    node.outbound << Edge.new(node, nil, "label")
+    node.outbound << Edge.new(node, nil, "nmod:to")
+    node.outbound << Edge.new(node, nil, "label")
+    node.outbound << Edge.new(node, nil, "nmod:to")
+
+    assert_equal([1, 3], node.nmod_indexes)
+  end
+
+  def test_nmod_points
+    node = Node.new("word", "pos", "lemma", 0)
+    nmod1 = Node.new("nmod1", "pos", "lemma", 1)
+    nmod2 = Node.new("nmod2", "pos", "lemma", 2)
+
+    node.outbound << Edge.new(node, nmod1, "nmod:to")
+    node.outbound << Edge.new(node, nmod2, "nmod:to")
+    nmod1.inbound << Edge.new(node, nmod1, "nmod:to")
+    nmod2.inbound << Edge.new(node, nmod2, "nmod:to")
+
+    points = node.nmod_points.map { |p| p.sort_by(&:index).map(&:word).join(" ") }
+
+    assert_equal(['word to nmod1', 'word to nmod2'], points)
+  end
 end
