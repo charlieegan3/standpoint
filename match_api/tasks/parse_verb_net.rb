888da1dba902366d0412342563427ac86a332a23
Charlie Egan <git@charlieegan3.com>
2016-02-11 12:30:20 +0000

    Include verbnet parser, now extracts full frame
    Motivation:
    -going to be making use of the syntax markup rather than the simplified
    primary string that was in use before.
    -should allow better tagging of matches (fewer errors and strange
    annotations)
    
    Change Explanation:
    -implement a simple task to parse the verbnet xml files and save the
    result as a json blob

---
+++ match_api/tasks/parse_verb_net.rb
@@ -0,0 +1,32 @@
+require 'json'
+require 'nokogiri'
+require 'pry'
+
+verbs = {}
+
+Dir.glob('verb_frames/*.xml') do |path|
+  doc = Nokogiri::XML(File.open(path).read)
+  members = doc.css('MEMBERS MEMBER').map { |e| e['name'] }
+
+  frames = doc.css('FRAMES FRAME').map do |frame|
+    examples = frame.css('EXAMPLE').map(&:text)
+    syntax = frame.css('SYNTAX > *').map do |component|
+      { name: component.name, value: component['value'],
+        restrictions: component.css('SYNRESTR').map{ |s| { value: s['value'], type: s['type'] } }}
+    end
+    semantics = frame.css('SEMANTICS > *').map do |predicate|
+      {
+        value: predicate['value'],
+        args: predicate.css('ARG').map { |a| { type: a['type'], value: a['value'] } }
+      }
+    end
+    { examples: examples, syntax: syntax, semantics: semantics }
+  end
+
+  members.each do |member|
+    verbs[member] = frames
+  end
+end
+
+File.open('verbs.json', 'w').write(verbs.to_json)
+puts "Wrote #{verbs.size} to verbs.json"
