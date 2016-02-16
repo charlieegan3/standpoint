5588eb44ecfc46f5b7f95b41cb904f02b8c1ba67
Charlie Egan <git@charlieegan3.com>
2016-02-16 17:32:47 +0000

    Incorporate 3 copula patterns
    Motivation:
    -from a sentiment analysis standpoint, as well as for coverage reasons,
    copula verbs needed to be covered.
    
    Change Explanation:
    -add 3 new queries for copula patterns
    -(based on corenlp parse of
    https://en.wikipedia.org/wiki/List_of_English_copulae)
    -these are attached to any 3 component frame for all copula verbs, this
    should be refined, perhaps with the addition of extra frames.
    -the results aren't correct. They just don't always tie with the frame
    that caused the match.

--- match_api/app.rb
+++ match_api/app.rb
@@ -20,9 +20,10 @@ neo4j_client = Neo4jClient.new("http://neo4j:7474")
 
 verbs = JSON.parse(File.open('verbs.json', 'r').read)
 frame_queries = Hash[*Dir.glob('frame_queries/*.cql').map do |path|
-  [path.scan(/\/(\w+)\./)[0][0].humanize.upcase,
+  [path.scan(/\/((\w|-)+)\./)[0][0].humanize.upcase.gsub('-COPULA', '-cop'),
     File.open(path, 'r').read]
 end.flatten]
+copulae = %w(act appear be become come end get go grow fall feel keep look prove remain run seem smell sound stay taste Turn wax)
 
 post '/' do
   sentence = JSON.parse(request.body.read)['sentence']
@@ -34,11 +35,19 @@ post '/' do
   neo4j_client.verbs.each do |verb|
     verbs[verb.lemma].each do |frame|
       frame = Frame.new(frame, verb.lemma)
-      next unless query = frame_queries[frame.pos_pattern_string]
-      match = neo4j_client.query(verb, query)
-      next if match.to_a.empty?
-      raise "Length Mismatch" + frame.pos_pattern_string unless match.size == frame.components.size
-      points << match.zip(frame.components).map { |m, c| { match: m, component: c } }
+      queries = []
+      if copulae.include?(verb.lemma) && frame.components.size == 3
+        queries += frame_queries.select { |k, _| k.include? '-cop' }.map(&:last)
+      end
+      queries << frame_queries[frame.pos_pattern_string]
+      queries.compact!
+      next if queries.empty?
+      queries.each do |query|
+        match = neo4j_client.query(verb, query)
+        next if match.to_a.empty?
+        raise "Length Mismatch" + frame.pos_pattern_string unless match.size == frame.components.size
+        points << match.zip(frame.components).map { |m, c| { match: m, component: c } }
+      end
     end
   end
   points.sort_by(&:size).to_json
