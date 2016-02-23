a43790ae39b5197b527f9f977f1ddf8f39be8ecd
Charlie Egan <git@charlieegan3.com>
2016-02-23 16:15:48 +0000

    Save & Group points based on their GR
    Motivation:
    - the queries return matching nodes
    - when writing queries nodes are named based on the associated
      grammatical relation
    - this information is useful for grouping points as it allows the
      arguments and verb types to be accounted for as part of the match.
    
    Change Explanation:
    - save the details of the matching relation along with the lemma
    - grouper makes use of this
    
    note: all still experimental, natural text is missing. Many relations
    could be considered equivalent to bolster group sizes.

--- analysis_api/src/analysis_api.cr
+++ analysis_api/src/analysis_api.cr
@@ -5,14 +5,14 @@ require "./analysis_api/*"
 
 module AnalysisApi
   def self.run
-    response = HTTP::Client.get "http://comment_store:3000/comments/754.json?flat=1"
+    response = HTTP::Client.get "http://comment_store:3000/comments/909.json?flat=1"
     blob = JSON.parse(response.body).as_a
       .map { |c| (c as Hash)["body"] as String }[1..-1]
       .map { |c| c.split("\n") }
       .flatten
       .select { |c| /^[A-Za-z]/.match(c) && c.split(" ").size > 4 }
       .map { |c| c[-1] == '.' ? c : c + "." }
-      .join(" ")
+      .take(LIMIT).join(" ")
 
     response = HTTP::Client.post("http://corenlp_server:9000/?properties=%7B%22annotators%22%3A%20%22tokenize%2Cssplit%22%7D", body: blob)
     clean = JSON.parse(response.body.gsub(/[^\w\{\}\]\[,:\s"\\']/, ""))
@@ -34,11 +34,13 @@ module AnalysisApi
       data = JSON.parse(response.body).as_a
       data.each do |point|
         next if (point as Array).size < 2
-        (point as Array).each do |c|
-          print (((((c as Hash)["match"] as Hash)["node"]  as Hash)["node"]) as Hash)["word"]
-          print " "
+        thing = (point as Array).map do |c|
+          [
+            (((((c as Hash)["match"] as Hash)["node"]  as Hash)["node"]) as Hash)["lemma"],
+            (((c as Hash)["match"] as Hash)["tag"] as String)
+          ].join(".")
         end
-        print "\n"
+        puts thing.to_json
       end
     end
   end
--- analysis_api/src/grouper.cr
+++ analysis_api/src/grouper.cr
@@ -1,24 +1,35 @@
+require "json"
+
 module Grouper
   def self.run
-    points = File.read_lines("points").map { |l| l.chomp.split(" ") }
-    groups = [] of Array(Array(String))
-
     topics = ["access", "apple", "bad", "case", "court", "data", "encryption", "fbi", "good", "government", "icloud", "nsa", "password", "people", "phone", "point", "precedent", "public", "secure", "security", "time"]
+    topics2 = ["arrington", "day", "hard", "hours", "jwz", "life", "lot", "make", "money", "people", "point", "smart", "startup", "time", "vc", "work", "worked", "working", "years"]
 
-    points = points.map { |p| p.join(" ") }.map(&.downcase)
+    points = File.read_lines("points2")
+    points = points.map {|p| JSON.parse(p).as_a.map(&.to_s) }
 
-    groups = topics.map do |t|
-      [t, points.select { |p| p.includes? t }]
-    end.sort_by(&.last.size).reverse
-
-    groups.each do |g|
-      puts g.first
-      puts g.last
-      puts
+    matched = [] of Array(String)
+    points.each_with_index do |p, i|
+      next if matched.includes? p
+      same = [] of Array(String)
+      points.each_with_index do |p2, i2|
+        next if i == i2
+        next if matched.includes? p2
+        if (p2 - p).size == 0 || (p - p2).size == 0
+          same << p2
+          matched << p
+          matched << p2
+        end
+      end
+      if same.size > 0
+        print i
+        puts p
+        same.each do |p|
+          puts "  " + p.to_s
+        end
+      end
     end
-
   end
-
 end
 
 Grouper.run
