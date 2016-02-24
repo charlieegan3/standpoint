52b8367a49075fd9894c9ae033cbc31de006da42
Charlie Egan <git@charlieegan3.com>
2016-02-24 17:43:48 +0000

    Investigate point grouping (verb & complete match)
    Motivation:
    -need to do this task manually to really work out what comes next but
    this can still help make the process faster. Grouping by verb for
    example is a good heuristic.
    
    Change Explanation:
    -find points with matching components
    -find points for a given verb
    -print them all out for evaluation

--- analysis_api/src/grouper.cr
+++ analysis_api/src/grouper.cr
@@ -1,32 +1,69 @@
 require "json"
 
+class Point
+  getter :string, :components
+  def initialize(string : String, components : Array)
+    @components, @string = components, string
+  end
+
+  def matches?(point : Point)
+    return false if point == self
+    (components - point.components).empty? &&
+    (point.components - components).empty?
+  end
+
+  def verb
+    components.select { |c| (c as String).includes? "verb" }.first.to_s
+  end
+
+  def includes?(lemma)
+    components.join(" ").includes?(lemma)
+  end
+end
+
 module Grouper
   def self.run
-    topics = ["access", "apple", "bad", "case", "court", "data", "encryption", "fbi", "good", "government", "icloud", "nsa", "password", "people", "phone", "point", "precedent", "public", "secure", "security", "time"]
-    topics2 = ["arrington", "day", "hard", "hours", "jwz", "life", "lot", "make", "money", "people", "point", "smart", "startup", "time", "vc", "work", "worked", "working", "years"]
+    points = File.read_lines("points2")[1..-1]
+    points = points.map {|p| JSON.parse(p).as_h }
+    points = points.map do |p|
+      Point.new(
+        (p["string"] as String),
+        (p["point"] as Array)
+      )
+    end
 
-    points = File.read_lines("points2")
-    points = points.map {|p| JSON.parse(p).as_a.map(&.to_s) }
-
-    matched = [] of Array(String)
-    points.each_with_index do |p, i|
-      next if matched.includes? p
-      same = [] of Array(String)
-      points.each_with_index do |p2, i2|
-        next if i == i2
-        next if matched.includes? p2
-        if (p2 - p).size == 0 || (p - p2).size == 0
-          same << p2
-          matched << p
-          matched << p2
+    matched = [] of Point
+    points.each_with_index do |point, index|
+      next if matched.includes? point
+      same = [] of Point
+      points.each_with_index do |inner_point, index|
+        if point.matches?(inner_point)
+          same << inner_point
+          matched << inner_point
         end
       end
-      if same.size > 0
-        print i
-        puts p
-        same.each do |p|
-          puts "  " + p.to_s
-        end
+
+      next if same.size < 2
+
+      puts point.components.join(", ")
+      puts "   " + point.string
+      same.each do |p|
+        puts "   " + p.string
+      end
+      puts
+    end
+
+    verb_groups = {} of String => Array(String)
+    verbs = (points.map(&.verb) as Array(String)).map(&.gsub(".verb", "")).uniq
+    verbs.each do |v|
+      verb_groups[v] = points.select(&.includes?(v)).map(&.string)
+    end
+
+    verb_groups.to_a.sort_by { |v| v[1].size }.each do |v|
+      puts v.first
+      v.last.each do |p|
+        print "     "
+        p.size > 120 ? puts p[0..120]+"..." : puts p
       end
     end
   end
