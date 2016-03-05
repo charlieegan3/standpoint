60c04a61b758c7511b48e8a6ed66715ac21170e2
Charlie Egan <git@charlieegan3.com>
2016-03-05 18:41:02 +0000

    Upgrade cleaning script to maintain post identity
    Motivation:
    -next step in the analysis requires stance information and or the user
    of the post to be retained.
    
    Change Explanation:
    -process corpus file into json key value string and write to a new file
    in the same directory instead.

--- utilities/clean.rb
+++ utilities/clean.rb
@@ -1,13 +1,30 @@
-folder_paths = %w(abortion  creation  gayRights  god  guns  healthcare).map {|f|"debates/#{f}/*"}
+require 'pry'
+require 'json'
+
+folder_paths = %w(abortion  creation  gayRights  god  guns  healthcare).map {|f|"analysis_api/debates/#{f}/*"}
 
 folder_paths.each do |path|
   puts path
-  Dir.glob(path) do |file|
-    file = File.open(file)
-    contents = file.readlines.reject { |x| x[0] == '#' }
-    contents.map! { |l| l.chars.select(&:valid_encoding?).join }
-    contents.map! { |l| l.gsub("/>", "") }
-    contents.map! { |l| l.gsub(" / ", "") }
-    File.open(file, "w").write(contents.join("\n"))
+  Dir.glob(path).to_a.each_with_index do |file, index|
+    content = File.open(file).read
+    content = content.chars.select(&:valid_encoding?).join
+
+    meta = Hash[*content.scan(/^#.*=.*$/).map { |e| e.delete('#').split("=") }.flatten]
+    meta = meta.inject({}){ |memo,(k,v)| memo[k.to_sym] = v; memo }.merge!(post: index)
+
+
+    content = content
+      .gsub(/^#.*=.*$/, "")
+      .gsub(/\[[0-9]+\]/, "")
+      .gsub(/http\S+|\S{30,}/, "")
+      .gsub(/([a-z]{3,}\.)([A-Z][a-z]{3,})/, '\1 \2')
+      .gsub(/([a-z]{3,}\.)([0-9]\.)/, '\1 \2')
+      .gsub(/\s+/, " ")
+      .gsub("[...]", "")
+      .gsub("/>", "")
+      .gsub(" / ", "")
+      .strip
+    processed_file = meta.merge(content: content)
+    File.open("#{file}.json", "w").write(processed_file.to_json)
   end
 end
