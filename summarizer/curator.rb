32a1f25cbb2614ffa1da3662f30a98228b7c51ee
Charlie Egan <git@charlieegan3.com>
2016-03-11 17:01:11 +0000

    Select extract based on group shared bigram count
    Motivation:
    -investigating new ways of generating summaries and selecting
    extracts/points
    
    Change Explanation:
    -collected and count the shared ngrams
    -score sentences based on the number of ngrams over their length

--- summarizer/curator.rb
+++ summarizer/curator.rb
@@ -18,9 +18,37 @@ module Curator
     end
   end
 
+  def self.sorted_dup_hash(array)
+    Hash[*array.inject(Hash.new(0)) { |h,e| h[e] += 1; h }.
+    select { |k,v| v > 1 }.inject({}) { |r, e| r[e.first] = e.last; r }.
+    sort_by {|_,v| v}.reverse.flatten(1)]
+  end
+
   def self.select_best(points)
     points = reparse_points(points.uniq { |p| p["String"] })
     permitted(points).min_by { |p| p["String"].length }
+    points = permitted(points)
+
+    group_bigrams = []
+    points.each do |p|
+      group_bigrams += bigrams(p["String"])
+    end
+    group_bigrams = sorted_dup_hash(group_bigrams)
+    points = points.sort_by do |p|
+      clean = p["String"].downcase.gsub(/[^\s\w']/, "").gsub(/\s+/, " ").strip
+      score = 0
+      group_bigrams.each do |k, v|
+        score += v if clean.include? k
+      end
+      p["Score"] = score.to_f / clean.split(" ").size
+    end.last
+  end
+
+  def self.bigrams(string)
+    words = string.downcase.gsub(/[^\s\w']/, "").split(/\s+/)
+    words.each_with_index.to_a[0..-2].map do |e, i|
+      [e, words[i+1]].join(" ")
+    end
   end
 
   def self.reparse_points(points)
@@ -35,13 +63,13 @@ module Curator
   def self.permitted(points)
     points.reject do |point|
       !point["String"].length.between?(15, 100) ||
+      #point["String"].match(/^(if|and|but|or|just|after|before|their|his|her|why)/i) ||
       point["String"].match(/this|And/) ||
       point["String"].match(/^\w+ ?, /) ||
       point["String"].match(/(\s*,){2}/) ||
       point["String"].match(/([A-Z]+ ){2,}/) ||
       point["String"].scan(/[a-z]+[0-9A-Z]/).any? ||
       point["String"].scan(/[LR]RB|[LR]SB/).size.odd? ||
-      #point["String"].match(/^(if|and|but|or|just|after|before|their|his|her|why)/i) ||
       point["Relations"].count { |r| r.match(/advcl|dep|acl|csubj|ccomp/) } > 0 ||
       point["Relations"].count { |r| r.match(/conj|nmod|acl/) } > 2 ||
       contains_case_change(point) ||
