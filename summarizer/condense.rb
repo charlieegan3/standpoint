cc9a1307039f28cf63c805c25d21938fa74e9a4e
Charlie Egan <git@charlieegan3.com>
2016-03-15 20:16:54 +0000

    Prevent point reuse in 'condensing'
    This prevents points being used in more than one condensed point (since
    this would mean that the group isn't really being condensed - as much).
    
    Condensed strings are also reformatted to remove -/+ markers that I
    thought I'd already removed.

--- summarizer/condense.rb
+++ summarizer/condense.rb
@@ -1,10 +1,12 @@
 module Condense
   def self.condense_group(group)
+    group.sort_by!(&:length)
     new_group = []
     merged = []
     group.uniq.combination(2).each do |s1, s2|
       s1 = s1.downcase.gsub(/[^\s\w']/, "").strip
       s2 = s2.downcase.gsub(/[^\s\w']/, "").strip
+      next unless ((merged + new_group) & [s1, s2]).empty?
       res = Differ.diff_by_word(s1, s2).to_s.gsub('"', "").gsub(" >> ", "|")
       change_count = res.scan(/\-|\+/).size
       group_count = res.scan(/\{[\w\|\s'\+\-]+\}/).size
@@ -21,10 +23,17 @@ module Condense
 
   def self.merge_diff_groups(string)
     match = string.scan(/\{.+\} \{.+\}/).first
-    return string if match.nil?
+    return format_match_string(string) if match.nil?
     g1, g2 = match.split("} {").map { |e| e.gsub(/\{|\}/, "").split("|") }
     replacement = g1.zip(g2).map { |g| g.join(" ") }.join("|")
-    string.gsub(match, "{#{replacement}}")
+    format_match_string(string.gsub(match, " { #{replacement} } "))
+  end
+
+  def self.format_match_string(string)
+    string.gsub(/\s*\}\s*/, "} ")
+      .gsub(/\s*\{\s*/, " {")
+      .gsub(/\s+/, " ")
+      .gsub(/\+|\-/, "")
   end
 
   def self.present_matched_string(string)
