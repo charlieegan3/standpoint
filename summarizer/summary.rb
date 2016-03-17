0d085cb295af9dbdd66836196c34d1cc82d73077
Charlie Egan <git@charlieegan3.com>
2016-03-17 17:20:58 +0000

    Limit returned point count for summary methods
    Adjust all methods to only return up to a maximum number of points. This
    keeps the output consistent and prevents all the points being processed.

--- summarizer/summary.rb
+++ summarizer/summary.rb
@@ -1,81 +1,105 @@
 class Summary
-  def initialize(title, points, topics)
-    @title, @points, @topics = title, points, topics
+  def initialize(title, points, topics, point_count)
+    @title, @points, @topics, @point_count = title, points, topics, point_count
     @groups = Hash[*points.group_by { |p| p["Components"] }.sort_by { |_, v| v.size }.reverse.flatten(1)]
   end
 
   def counter_points
+    count = 0
     Counters.counter_points(@points).map do |point, counter|
+      next if count >= @point_count
       point = Curator.select_best(@groups[point])
       counter = Curator.select_best(@groups[counter])
       next if [point, counter].map(&:nil?).any?
-      condensed = Condense.condense_group([point["String"], counter["String"]])
-      condensed.size == 1 ? [true, point, counter] : [false, point, counter]
+      count += 1
+      [point, counter]
     end.compact
   end
 
   def related_points
+    count = 0
     Related.related_points(@points).map do |point, related|
+      next if count >= @point_count
       point = Curator.select_best(@groups[point])
       related = Curator.select_best(@groups[related])
       next if [point, related].map(&:nil?).any?
+      count += 1
       [point, related]
     end.compact
   end
 
   def negated_points
+    count = 0
     @groups.map do |_, group|
+      next if count >= @point_count
       counter_point_groups = Counters.negated_points(group)
       next if counter_point_groups.empty?
       best = counter_point_groups.map { |g| g.min_by { |r, _, _| r.length } }.min_by { |r, _, _| r.scan(/\||\{|\}/).size }
       next unless best
+      count += 1
       best[0] = Curator.clean_string(Condense.format_match_string(Condense.merge_diff_groups(best.first)))
-      puts best
+      best
     end.compact
   end
 
   def common_points
+    count = 0
     @groups.map do |k, g|
+      next if count >= @point_count
+      count += 1
       Curator.select_best(g)
     end.compact
   end
 
   def longer_points
     # needs condensing
-    @groups.reject { |k, v| k.size < 4 || v.size < 3 }.sort_by { |_, v| v.size }.reverse.map do |k, g|
-      Curator.select_best(g)
-    end.compact.uniq { |p| p["String"] }
+    count = 0
+    [].tap do |points|
+      @groups.reject { |k, v| k.size < 4 || v.size < 3 }.sort_by { |_, v| v.size }.reverse.map do |k, g|
+        next if count >= @point_count
+        best = Curator.select_best(g)
+        next if points.map { |p| p["String"] }.include? best["String"]
+        count += 1
+        points << best
+      end
+    end
   end
 
   def commonly_discussed_topic_points
     # needs condensing
-    top_topics.take(10).map do |t|
-      points = groups.select { |k, _| k.join(" ").include? " #{t}." }.take(5).map do |k, group|
+    count = 0
+    top_topics.map do |t|
+      next if count >= @point_count
+      points = @groups.select { |k, _| k.join(" ").include? " #{t}." }.take(5).map do |k, group|
         point = Curator.select_best(group)
       end.compact
+      count += 1
       [t, points]
-    end
+    end.compact
   end
 
   def multiple_topic_points
     # needs condensing
-    topic_points = @points.sort_by { |p| topics.count { |t| p["String"].downcase.include? t } }.reverse.take(100)
+    topic_points = @points.sort_by { |p| @topics.count { |t| p["String"].downcase.include? t } }.reverse.take(100)
     topic_points.uniq! { |p| p["String"] }
     selected_points = []
     for i in 0..10
       selected_points << topic_points.delete(Curator.select_best(topic_points))
-      break if selected_points.size > 5
+      break if selected_points.size >= @point_count
     end
     selected_points
   end
 
   def question_points
-    question_groups = points.select { |p| p["String"].include? "?" }
+    count = 0
+    question_groups = @points.select { |p| p["String"].include? "?" }
           .uniq {|p| p["String"] }
           .group_by { |p| p["Components"] }
           .sort_by { |k, v| v.size }
           .select { |k, v| v.size > 2 }
     question_groups.map do |_, group|
+      next if count >= @point_count
+      count += 1
       Curator.select_best_question(group)
     end.compact
   end
