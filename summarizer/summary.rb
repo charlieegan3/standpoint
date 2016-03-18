41397486d0e769cc18a758ce4627df7ce3407820
Charlie Egan <git@charlieegan3.com>
2016-03-18 15:42:50 +0000

    Prevent point reuse in summaries
    Points are spent as they are included in the summary, earlier sections
    have greater precedence as they dip into points list first. Point groups
    are still not condensed on the front end.

--- summarizer/summary.rb
+++ summarizer/summary.rb
@@ -1,30 +1,35 @@
 class Summary
   attr_accessor :topics
+
   def initialize(title, points, topics, point_count)
     @title, @points, @topics, @point_count = title, points, topics, point_count
     @groups = Hash[*points.group_by { |p| p["Components"] }.sort_by { |_, v| v.size }.reverse.flatten(1)]
+    @used_points = []
   end
 
   def counter_points
     count = 0
-    Counters.counter_points(@points).map do |point, counter|
+    Counters.counter_points(available_points).map do |point, counter|
       next if count >= @point_count
       point = Curator.select_best(@groups[point])
       counter = Curator.select_best(@groups[counter])
       next if [point, counter].map(&:nil?).any?
       count += 1
+      use_point(point); use_point(counter)
       [point, counter]
     end.compact
   end
 
   def related_points
     count = 0
-    Related.related_points(@points).map do |point, related|
+    Related.related_points(available_points).map do |point, related|
       next if count >= @point_count
       point = Curator.select_best(@groups[point])
       related = Curator.select_best(@groups[related])
       next if [point, related].map(&:nil?).any?
+      next unless (available_points & [point, related]).size == 2
       count += 1
+      use_point(point); use_point(related)
       [point, related]
     end.compact
   end
@@ -33,22 +38,26 @@ class Summary
     count = 0
     @groups.map do |_, group|
       next if count >= @point_count
-      counter_point_groups = Counters.negated_points(group)
+      counter_point_groups = Counters.negated_points(available_points(group))
       next if counter_point_groups.empty?
       best = counter_point_groups.map { |g| g.min_by { |r, _, _| r.length } }.min_by { |r, _, _| r.scan(/\||\{|\}/).size }
       next unless best
       count += 1
+      use_point(best[1]); use_point(best[2])
       best[0] = Curator.clean_string(Condense.format_match_string(Condense.merge_diff_groups(best.first)))
       best
     end.compact
   end
 
   def common_points
+    # needs condensing
     count = 0
     @groups.map do |k, g|
       next if count >= @point_count
+      best = Curator.select_best(available_points(g))
+      next if best.nil?
       count += 1
-      Curator.select_best(g)
+      use_point(best)
     end.compact
   end
 
@@ -58,7 +67,7 @@ class Summary
     [].tap do |points|
       @groups.reject { |k, v| k.size < 4 || v.size < 3 }.sort_by { |_, v| v.size }.reverse.map do |k, g|
         next if count >= @point_count
-        best = Curator.select_best(g)
+        best = Curator.select_best(available_points(g))
         next if points.map { |p| p["String"] }.include? best["String"]
         count += 1
         points << best
@@ -114,6 +123,27 @@ class Summary
   end
 
   private
+
+  def available_points(points=@points)
+    points.select { |p| (@used_points & point_id_strings(p)).empty? }
+  end
+
+  def use_point(point)
+    @used_points += point_id_strings(point)
+    point
+  end
+
+  def point_id_strings(point)
+    if point["Lemmas"]
+      lemma_string = point["Lemmas"].select { |l| l.match(/[a-z]/) }.map { |l| l.split(":").first }.join(" ").downcase
+    end
+    [
+      point["String"].gsub(/[^\w\s]+/, "").gsub(/\s+/, " ").strip.downcase,
+      point["Components"].join(" "),
+      Presenter.format(point["String"], @topics),
+      lemma_string
+    ].compact
+  end
 
   def top_topics
     Utils.sorted_dup_hash(@groups.keys.flatten.map(&:downcase))
