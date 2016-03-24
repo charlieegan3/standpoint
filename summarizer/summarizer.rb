a6aa752cc0b9eb91675b52e7d168a9678facd4b9
Charlie Egan <git@charlieegan3.com>
2016-03-24 23:28:33 +0000

    Output summaries as side-by-side comparisons
    As part of the survey summaries should be compared side by side and in
    the same format. The summarizer now generates output in this form.

--- summarizer/summarizer.rb
+++ summarizer/summarizer.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'differ'
 require 'levenshtein'
 require 'erb'
+require 'nokogiri'
 
 require_relative 'summary'
 
@@ -24,8 +25,16 @@ summary.build
 
 @title = title
 @summary = summary
+@stock_summary = ""
+
 erb = ERB.new(File.open("template.html.erb").read, 0, '>')
 html = erb.result binding
-File.open(title.downcase.gsub(/\W+/, "_") + "_summary.html", "w").write(html)
+word_count = Nokogiri::HTML(html).text.split(/\s+/).size
 
-File.open(title.downcase.gsub(/\W+/, "_") + "_summary.json", "w").write(summary.to_h.to_json)
+file_name = title.split(" ").map(&:capitalize).join
+file_name = file_name[0].downcase + file_name[1..-1]
+@stock_summary = `python ../stock_summarizers/nlp_course/summarizer_topic.py #{file_name} #{word_count}`
+
+html = erb.result binding
+File.open(title.downcase.gsub(/\W+/, "_") + "_summary.html", "w") { |file| file.write(html) }
+File.open(title.downcase.gsub(/\W+/, "_") + "_summary.json", "w") { |file| file.write(summary.to_h.to_json) }
