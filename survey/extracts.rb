f5aa056ada29d111aea33577e2ae8d07ed07a3ee
Charlie Egan <git@charlieegan3.com>
2016-04-07 14:31:53 +0100

    Adjustments to survey wording

--- survey/extracts.rb
+++ survey/extracts.rb
@@ -12,7 +12,9 @@ topics = %w(abortion creation guns god gay_rights healthcare)
 contents = File.open('extracts.txt').read
 groups = contents.split("------").map { |e| e.split("\n").select { |l| l.match(/^\w/) } }
 
+index = 0
 topics.zip(groups.reject(&:empty?)).each do |topic, extracts|
   erb = ERB.new(File.open("survey_extract.html.erb").read, 0, '>')
   File.open("#{topic}.html", "w") { |file| file.write(erb.result(binding)) }
+  index += 1
 end
