bfa25dcc8b21a08785e54b0043f02fa353fad5c6
charlieegan3 <charlieegan3@googlemail.com>
2016-04-11 21:22:53 +0100

    Add scripts for processing tweet data
    Testing the tool on a new corpus

---
+++ utilities/clean_tweets.rb
@@ -0,0 +1,15 @@
+require 'pry'
+require 'json'
+
+folder_paths = %w(yes no).map { |f| "../analysis_api/tweets/#{f}/*" }
+
+folder_paths.each do |path|
+  puts path
+  Dir.glob(path).to_a.each do |file, index|
+    content = File.open(file).read.chars.select(&:valid_encoding?).join
+    content.gsub!(/\s+/, " ")
+    id = file.split("/").last.gsub(".txt", "").gsub(/\W/, "").strip
+
+    File.open("#{file.gsub(".txt", "")}.json", "w").write({ content: content, post: id }.to_json)
+  end
+end
