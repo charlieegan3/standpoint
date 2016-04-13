3f5b69b67f1384d592cf1cb517a3f88e65d95a76
Charlie Egan <git@charlieegan3.com>
2016-04-13 16:21:44 +0100

    Print data for boxplots when comparing extracts
    The box plot isn't all that clear but this if how you get the data.

--- evaluation/extract_ranking.rb
+++ evaluation/extract_ranking.rb
@@ -1,5 +1,25 @@
 require 'pry'
 
+module Enumerable
+  def sum
+	self.inject(0){|accum, i| accum + i }
+  end
+
+  def mean
+	self.sum/self.length.to_f
+  end
+
+  def sample_variance
+	m = self.mean
+	sum = self.inject(0){|accum, i| accum +(i-m)**2 }
+	sum/(self.length - 1).to_f
+  end
+
+  def standard_deviation
+	return Math.sqrt(self.sample_variance)
+  end
+end
+
 def sorted_dup_hash(array)
   Hash[*array.inject(Hash.new(0)) { |h,e| h[e] += 1; h }.
     select { |k,v| v > 1 }.inject({}) { |r, e| r[e.first] = e.last; r }.
@@ -43,4 +63,23 @@ human_scored_extracts = Hash[*File.open('scored_extracts.txt').readlines.map { |
 bigram_scored_extracts.each do |k, v|
   raise if human_scored_extracts[k].nil?
   puts [v, human_scored_extracts[k]].join(", ")
+end
+
+machine_mean = bigram_scored_extracts.values.mean
+machine_stdev = bigram_scored_extracts.values.standard_deviation
+
+machine_for_each_human_score = Hash.new([])
+File.open('scored_extracts_all.txt').readlines.map { |l| l.split("||") }.each do |e, scores|
+  next unless e.strip.length > 0
+  scores.strip.split(",").map(&:to_i).each do |s|
+    machine_score = bigram_scored_extracts[e]
+    raise if machine_score.nil?
+    machine_for_each_human_score[s] += [((machine_score - machine_mean) / machine_stdev).round(2)]
+  end
+end
+
+machine_for_each_human_score[1] += machine_for_each_human_score.delete(0)
+
+machine_for_each_human_score.sort_by { |k, _| k }.each do |k, v|
+  puts "r#{k} = c(#{v.join(",")})"
 end
