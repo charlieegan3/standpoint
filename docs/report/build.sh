5b1db6da627a12420779abae02e81a6cbba24366
Charlie Egan <git@charlieegan3.com>
2016-04-22 21:32:12 +0100

    Capitalize Stock, Plain, Formatted

--- docs/report/build.sh
+++ docs/report/build.sh
@@ -1,9 +1,12 @@
 osascript -e 'quit app "PDF Expert"'
 
+rm graphs/*.pdf
+cp ../../evaluation/r_analysis/*.pdf graphs/
+
 pdflatex abdnthesis
-bibtex abdnthesis
-pdflatex abdnthesis
-pdflatex abdnthesis
+#bibtex abdnthesis
+#pdflatex abdnthesis
+#pdflatex abdnthesis
 
 rm -f *.aux *.bbl *.blg *.dvi *.log *.toc
 
