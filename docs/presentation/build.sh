adc51f46a741862dfbba18d9d6cf31a2f9cc49f2
Charlie Egan <git@charlieegan3.com>
2016-04-29 22:51:27 +0100

    After submission purge
    Remove all the evaluation and docs material

--- docs/presentation/build.sh
+++
@@ -1,6 +0,0 @@
-osascript -e 'quit app "PDF Expert"'
-
-pdflatex presentation
-open presentation.pdf
-
-rm -f *.aux *.bbl *.blg *.dvi *.log *.toc *.snm *.out *.nav
--- evaluation/evaluation_sheet/build.sh
+++
@@ -1,6 +0,0 @@
-osascript -e 'quit app "PDF Expert"'
-
-pdflatex $1
-open $1.pdf
-
-rm -f *.aux *.bbl *.blg *.dvi *.log *.toc *.snm *.out *.nav
