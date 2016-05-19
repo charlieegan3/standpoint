81dbbe4d565ca38df5f5554c09ea0a4c6b0aa85b
Charlie Egan <git@charlieegan3.com>
2016-05-19 23:26:04 +0100

    Implement HTTP post to corenlp
    This is where the graph is going to come from and so this step is
    required to move on. This is going to talk to corenlp alone in place of
    the points_api.
    
    In the future, ideally, the dependency parser would be contained within
    the same container to avoid the request response overhead.

--- point_extractor/src/main.rs
+++ point_extractor/src/main.rs
@@ -1,40 +1,23 @@
 extern crate graph_match;
-mod graph_parser;
+extern crate hyper;
+
+use hyper::Url;
+use hyper::client::Request;
+use std::io::Read;
 
 fn main() {
-    let string_graph =
-        "type:node identifier:node0 pos:noun word:charlie\n\
-         type:node identifier:node1 pos:verb word:went\n\
-         type:node identifier:node2 pos:noun word:home\n\
-         type:node identifier:node3 pos:det word:the\n\
-         type:edge identifier:subj source:1 target:0 key:value\n\
-         type:edge identifier:obj source:1 target:2 key:value\n\
-         type:edge identifier:det source:0 target:3 key:value";
-    let mut graph: graph_match::graph::Graph;
-    match graph_parser::parse(string_graph.to_string()) {
-        Ok(g) => graph = g,
-        Err(message) => panic!(message),
-    }
+    let mut url = Url::parse("http://corenlp_server:9000").unwrap();
+    url.query_pairs_mut().append_pair("properties", "{\"annotators\": \"lemma,tokenize,ssplit,depparse\"}");
+    println!("{}", url.as_str());
 
-    let query_string =
-        "type:node identifier:node0 pos:noun\n\
-         type:node identifier:node1 pos:verb\n\
-         type:node identifier:node2 pos:noun\n\
-         type:edge identifier:subj source:1 target:0\n\
-         type:edge identifier:obj source:1 target:2";
-    let query = graph_parser::parse(query_string.to_string());
+    let client = hyper::Client::new();
 
-    for component in graph_match::match_graph(&query.unwrap(), 1, &graph).list {
-        match graph.nodes[component.node].attributes {
-            Some(ref attrs) => println!("{:?}", attrs),
-            None => println!("There were no attrs"),
-        }
-    }
-    println!("---");
-    for index in graph_match::expand_subgraph(&graph, 1, &vec![]) {
-        match graph.nodes[index].attributes {
-            Some(ref attrs) => println!("{:?}", attrs),
-            None => println!("There were no attrs"),
-        }
-    }
+    let mut res = client.post(url.as_str())
+        .body("This is a sentence about a dog.")
+        .send()
+        .unwrap();
+
+    let mut body = String::new();
+    res.read_to_string(&mut body);
+    println!("{}", body);
 }
