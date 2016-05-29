cacc1f880352d2cbdb2bb63ee440e739eb8cdd29
Charlie Egan <git@charlieegan3.com>
2016-05-29 22:55:24 +0100

    Convert a number of Cypher frames
    These were previously listed in the points_api under frame queries. This
    is the new representation for such queries.
    
    This way they are part of the tool and do not need to be interpreted
    each time the query is run.

--- point_extractor/src/frames.rs
+++ point_extractor/src/frames.rs
@@ -1,11 +1,18 @@
 use std::collections::HashMap;
 use graph_match::graph::Graph;
-use graph_parser;
+us graph_parser;
 
 pub fn frame_index() -> HashMap<String, (usize, Graph)> {
     let mut index: HashMap<String, (usize, Graph)> = HashMap::new();
     let frames = vec![
-        np_v_np()
+        np_v(),
+        np_v_np(),
+        np_v_adj(),
+        np_v_adv(),
+        np_v_adv_lex(),
+        np_v_adv_prep_np(),
+        lex_v_np_prep_np(),
+        lex_v_prep_np_np(),
     ];
 
     for frame in frames {
@@ -22,3 +29,84 @@ fn np_v_np() -> (String, usize, Graph) {
                         type:edge identifier:obj source:1 target:2 label:dobj";
     return (String::from("NP VERB NP"), 1, graph_parser::parse(&query_string.to_string()).unwrap());
 }
+
+fn np_v() -> (String, usize, Graph) {
+    let query_string = "type:node identifier:subj\n\
+                        type:node identifier:verb pos:VB\n\
+                        type:edge identifier:obj source:1 target:0 label:nsubj";
+    return (String::from("NP VERB"), 1, graph_parser::parse(&query_string.to_string()).unwrap());
+}
+
+fn np_v_adv() -> (String, usize, Graph) {
+    let query_string = "type:node identifier:subj\n\
+                        type:node identifier:verb pos:VB\n\
+                        type:node identifier:adv\n\
+                        type:edge identifier:subj source:1 target:0 label:nsubj\n\
+                        type:edge identifier:advmod source:1 target:2 label:advmod";
+    return (String::from("NP VERB ADV"), 1, graph_parser::parse(&query_string.to_string()).unwrap());
+}
+
+fn np_v_adv_prep_np() -> (String, usize, Graph) {
+    let query_string = "type:node identifier:subj\n\
+                        type:node identifier:verb pos:VB\n\
+                        type:node identifier:adv\n\
+                        type:node identifier:nmod\n\
+                        type:node identifier:prep\n\
+                        type:edge identifier:subj source:1 target:0 label:nsubj\n\
+                        type:edge identifier:advmod source:1 target:2 label:advmod\n\
+                        type:edge identifier:nmod source:1 target:3 label:nmod\n\
+                        type:edge identifier:case source:3 target:4 label:case";
+    return (String::from("NP VERB ADV PREP NP"), 1, graph_parser::parse(&query_string.to_string()).unwrap());
+}
+
+// lex should be before avd
+fn np_v_adv_lex() -> (String, usize, Graph) {
+    let query_string = "type:node identifier:subj\n\
+                        type:node identifier:verb pos:VB\n\
+                        type:node identifier:dobj\n\
+                        type:node identifier:adv\n\
+                        type:node identifier:lex\n\
+                        type:edge identifier:subj source:1 target:0 label:nsubj\n\
+                        type:edge identifier:obj source:1 target:2 label:dobj\n\
+                        type:edge identifier:adv source:1 target:3 label:advmod\n\
+                        type:edge identifier:lex source:1 target:4 label:advmod";
+    return (String::from("NP VERB ADV LEX"), 1, graph_parser::parse(&query_string.to_string()).unwrap());
+}
+
+fn lex_v_np_prep_np() -> (String, usize, Graph) {
+    let query_string = "type:node identifier:lex\n\
+                        type:node identifier:verb pos:VB\n\
+                        type:node identifier:obj\n\
+                        type:node identifier:prep\n\
+                        type:node identifier:nmod\n\
+                        type:edge identifier:expl source:1 target:0 label:expl\n\
+                        type:edge identifier:dobj source:1 target:2 label:dobj\n\
+                        type:edge identifier:nmod source:1 target:3 label:nmod\n\
+                        type:edge identifier:case source:3 target:4 label:case";
+    return (String::from("LEX VERB NP PREP NP"), 1, graph_parser::parse(&query_string.to_string()).unwrap());
+}
+
+// uses generic dependency, this should not expand.
+fn lex_v_prep_np_np() -> (String, usize, Graph) {
+    let query_string = "type:node identifier:lex\n\
+                        type:node identifier:verb pos:VB\n\
+                        type:node identifier:nmod\n\
+                        type:node identifier:prep\n\
+                        type:node identifier:dep\n\
+                        type:edge identifier:expl source:1 target:0 label:expl\n\
+                        type:edge identifier:nmod source:1 target:2 label:nmod\n\
+                        type:edge identifier:case source:2 target:3 label:case\n\
+                        type:edge identifier:dep source:2 target:4 label:dep";
+    return (String::from("LEX VERB PREP NP NP"), 1, graph_parser::parse(&query_string.to_string()).unwrap());
+}
+
+// copula frames
+fn copula_np_v_adj() -> (String, usize, Graph) {
+    let query_string = "type:node identifier:subj\n\
+                        type:node identifier:verb pos:VB\n\
+                        type:node identifier:adj\n\
+                        type:edge identifier:subj source:1 target:0 label:nsubj\n\
+                        type:edge identifier:xcomp source:1 target:2 label:xcomp";
+    return (String::from("copula:NP VERB ADJ"), 1, graph_parser::parse(&query_string.to_string()).unwrap());
+}
+
