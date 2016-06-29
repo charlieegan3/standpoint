5cc6fa87d1ba5be5a87352e101dc8ba0ee12c2c8
Charlie Egan <git@charlieegan3.com>
2016-06-29 23:54:01 +0100

    Validate against a list of banned patterns
    Longer patterns that accompany problematic extracts now also lead to the
    rejection of points.

--- point_extractor/src/formatting.rs
+++ point_extractor/src/formatting.rs
@@ -3,11 +3,11 @@ use regex::Regex;
 pub fn pattern(pattern: String) -> String {
     let pattern = pattern.to_lowercase();
     let pattern = merge_person_nouns(pattern);
-    let pattern = String::from(pattern.trim_right());
+    let pattern = String::from(pattern.trim());
     return pattern;
 }
 
 fn merge_person_nouns(pattern: String) -> String {
-    let re = Regex::new(r"(^|\s)(i|you|me|who|we|you|they|them|he|she|person|people).\w+(\s|$)").unwrap();
-    return re.replace_all(pattern.as_str(), "PERSON.noun ");
+    let re = Regex::new(r"(^|\s)(i|you|me|who|we|you|yourself|they|them|he|she|person|people).\w+(\s|$)").unwrap();
+    return re.replace_all(pattern.as_str(), " PERSON.noun ");
 }
--- point_extractor/src/validate.rs
+++ point_extractor/src/validate.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 pub fn pattern(pattern: &String) -> bool {
-    return !is_banned_speech_act(pattern);
+    return !is_banned_speech_act(pattern) && !is_banned_pattern(pattern);
 }
 
 fn is_banned_speech_act(pattern: &String) -> bool {
@@ -10,4 +10,50 @@ fn is_banned_speech_act(pattern: &String) -> bool {
     }
     let re = Regex::new(r"\s(object|continue|come|go|sit|open|close|begin|end|believe|happen|leave|understand|realize|debate|speak|show|stand|call|refer|believe|lose|change|care|hear|write|disagree|read|tell|start|talk|explain|come|live|take|support|guess|feel|follow|make|go|get|move|agree|find|fail|think|wonder|feel|ask|argue|try)\.").unwrap();
     return re.is_match(pattern);
+}
+
+fn is_banned_pattern(pattern: &String) -> bool {
+    if vec![
+        "PERSON.noun be.verb correct.adj",
+        "PERSON.noun be.verb able.adj",
+        "PERSON.noun be.verb good.adj",
+        "PERSON.noun be.verb likely.adj",
+        "PERSON.noun be.verb sorry.adj",
+        "PERSON.noun be.verb say.verb",
+        "PERSON.noun be.verb aware.adj",
+        "PERSON.noun be.verb one.noun",
+        "PERSON.noun be.verb sure.adj",
+        "PERSON.noun be.verb wrong.adj",
+        "PERSON.noun be.verb right.adj",
+        "PERSON.noun be.verb glad.adj",
+        "PERSON.noun be.verb here.noun",
+        "PERSON.noun be.verb willing.verb",
+        "PERSON.noun be.verb true.adj",
+        "PERSON.noun be.verb false.adj",
+        "PERSON.noun be.verb favor.noun",
+        "PERSON.noun be.verb interested.adj",
+        "PERSON.noun want.verb have.verb",
+        "PERSON.noun want.verb what.wp",
+        "PERSON.noun want.verb what.wp do.verb",
+        "PERSON.noun say.verb what.wp",
+        "PERSON.noun mean.verb what.wp",
+        "PERSON.noun know.verb what.wp",
+        "PERSON.noun believe.verb what.wp",
+        "PERSON.noun see.verb what.wp",
+        "PERSON.noun see.verb argument.noun",
+        "PERSON.noun have.verb problem.noun",
+        "PERSON.noun tell.verb PERSON.noun",
+        "PERSON.noun think.verb what.wp",
+        "PERSON.noun argue.verb in.prep fact.noun",
+        "PERSON.noun argue.verb with.prep PERSON.noun",
+        "debate.noun be.verb about.adj",
+        "question.noun be.verb",
+        "make.verb claim.noun",
+        "ask.verb PERSON.noun",
+        "thing.noun happen.verb",
+        "something.noun happen.verb"].contains(&pattern.as_str()) {
+            return true;
+        } else {
+            return false;
+        }
 }
