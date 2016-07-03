5a60b802e360ddb48f59f4e0ee0b4ac29c858757
Charlie Egan <git@charlieegan3.com>
2016-07-03 10:30:45 +0100

    Add d3 force graph
    This is likely to be replaceed by a chord plot for clarity.

---
+++ standpoint_server/app/assets/javascripts/graph.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+  var graph_data = document.getElementById("graph_data").innerHTML;
+  var graph = JSON.parse(graph_data);
+
+  var width = 500,
+      height = 500;
+
+  var color = d3.scale.category20();
+
+  var force = d3.layout.force()
+      .charge(-120)
+      .linkDistance(200)
+      .size([width, height]);
+
+  var svg = d3.select("#graph")
+	  .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  force.nodes(graph.nodes)
+      .links(graph.links)
+      .start();
+
+  var link = svg.selectAll(".link")
+      .data(graph.links)
+      .enter().append("line")
+      .attr("class", "link")
+      .style("stroke-width", "1px")
+      .style("stroke", "#d9d9d9");
+
+  var node = svg.selectAll(".node")
+      .data(graph.nodes)
+      .enter().append("g")
+      .attr("class", "node")
+      .call(force.drag)
+      .on("mousedown", function(d) {
+        d.fixed = true;
+        d3.select(this).classed("sticky", true);
+      });
+
+  node.append("circle")
+      .attr("r", function (d) {
+        return Math.log(d.value) * 5;
+      })
+      .style("fill", function (d) {
+        return color(d.group);
+      });
+
+  node.append("text")
+        .attr("text-anchor", "middle")
+        .attr("dy", 1.5)
+        .text(function(d) { return d.name });
+
+  force.on("tick", function () {
+      link.attr("x1", function (d) { return d.source.x; })
+          .attr("y1", function (d) { return d.source.y; })
+          .attr("x2", function (d) { return d.target.x; })
+          .attr("y2", function (d) { return d.target.y; });
+
+      d3.selectAll("circle")
+          .attr("cx", function (d) { return d.x; })
+          .attr("cy", function (d) { return d.y; });
+
+      d3.selectAll("text")
+          .attr("x", function (d) { return d.x; })
+          .attr("y", function (d) { return d.y; });
+  });
+});
--- standpoint_server/app/models/discussion.rb
+++ standpoint_server/app/models/discussion.rb
@@ -13,7 +13,7 @@ class Discussion < ActiveRecord::Base
       .reverse
   end
 
-  def point_pattern_graph
+  def graph_data
     nodes, edges = [], []
     point_clusters.each do |k, v|
       v.each do |point|
@@ -22,6 +22,17 @@ class Discussion < ActiveRecord::Base
         (0...tokens.size - 1).to_a.each { |i| edges << tokens[i, 2] }
       end
     end
-    return { nodes: nodes, edges: edges }
+    name_index = Hash.new()
+    nodes = Utils.sorted_dup_hash(nodes).take(25).each_with_index.to_a.map do |e, i|
+      name, type = e[0].split(".")
+      name_index[e[0]] = i
+      { name: name, group: type, value: e[1] }
+    end
+    edges = Utils.sorted_dup_hash(edges).map do |k, v|
+	  source, target = name_index[k.first], name_index[k.last]
+      next if source.nil? || target.nil?
+      { source: name_index[k.first], target: name_index[k.last], value: v }
+    end.compact
+    return { nodes: nodes, links: edges }
   end
 end
