2e13a532a78bcce89c749d58a7bce6cc90068f60
Charlie Egan <git@charlieegan3.com>
2016-07-09 12:37:31 +0100

    Correct HN top collector URL
    I thought the dom paths were absolute.

--- standpoint_server/lib/hacker_news_current_top_collector.rb
+++ standpoint_server/lib/hacker_news_current_top_collector.rb
@@ -5,7 +5,7 @@ require_relative '../config/environment'
 class HackerNewsCurrentTopCollector
   def perform
     doc = Nokogiri::HTML(open("https://news.ycombinator.com").read)
-    url = "https://news.ycombinator.com" +
+    url = "https://news.ycombinator.com/" +
       doc.at_css(".subtext a:last-child")['href']
     existing.destroy if existing = Discussion.find_by_url(url)
     HackerNewsCollector.new.perform(url)
