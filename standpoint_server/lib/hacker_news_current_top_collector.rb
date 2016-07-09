78219f79976f0f6bf550e3601d830beb12068230
Charlie Egan <git@charlieegan3.com>
2016-07-09 12:01:09 +0100

    Auto collect HN threads using full URL
    This is used as a link out to the original story.

--- standpoint_server/lib/hacker_news_current_top_collector.rb
+++ standpoint_server/lib/hacker_news_current_top_collector.rb
@@ -5,7 +5,8 @@ require_relative '../config/environment'
 class HackerNewsCurrentTopCollector
   def perform
     doc = Nokogiri::HTML(open("https://news.ycombinator.com").read)
-    HackerNewsCollector.new.perform(doc.at_css(".subtext a:last-child")['href'])
+    path = doc.at_css(".subtext a:last-child")['href']
+    HackerNewsCollector.new.perform("https://news.ycombinator.com#{path}")
   end
 
   handle_asynchronously :perform
